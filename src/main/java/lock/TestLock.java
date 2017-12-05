64a3a79c78792c197b28edb848d84e3318841b1b
sonnyching <sonnyching@outlook.com>
2017-12-05 23:53:36 +0800

    add test

--- src/main/java/lock/TestLock.java
+++ src/main/java/lock/TestLock.java
@@ -8,7 +8,17 @@ import java.util.concurrent.TimeUnit;
 public class TestLock {
 
     public static void main(String[] args) {
-        testShareLock();
+
+        String newString = new String("323");
+        synchronized (newString){
+            try {
+                newString.notify();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+//        testJoin();
     }
 
     public static void testShareLock(){
@@ -61,6 +71,48 @@ public class TestLock {
 
         System.out.println("end-------");
 
+
+    }
+
+
+    private static class stopThread extends  Thread{
+        private boolean isRunning = true;
+
+        @Override
+        public void run() {
+            while(isRunning && !Thread.currentThread().isInterrupted()){
+                System.out.println("i am running~~~");
+            }
+        }
+
+        public void stopThread(){
+            this.isRunning = false;
+        }
+
+    }
+
+
+    private static void testJoin(){
+        Thread b = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    System.out.println("B线程结束啦");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        b.start();
+
+        try {
+            b.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("主线程结束啦");
 
     }
 
